6ed2d1536184a74b93534f4dfd6678bf56e21b67
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-10-09 01:48:08 +0530

    connected to mongo atlas

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/civic-connect";
+  process.env.MONGODB_URI ||
+  "mongodb+srv://jagdeeps3105:jagdeeps3105@cluster0.hn1cynq.mongodb.net/";
 
 export const connectDatabase = async (): Promise<void> => {
   try {
